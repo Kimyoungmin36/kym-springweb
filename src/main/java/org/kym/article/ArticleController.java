0b17ff4cbe66147ca29ace8eeb6c57a8e910f220
Kimyoungmin36 <pon36@naver.com>
2019-05-31 09:54:52 +0900

    fifth commit

--- src/main/java/org/kym/article/ArticleController.java
+++ src/main/java/org/kym/article/ArticleController.java
@@ -2,6 +2,10 @@ package org.kym.article;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kym.book.chap11.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +20,8 @@ public class ArticleController {
 
 	@Autowired
 	ArticleDao articleDao;
+
+	static final Logger logger = LogManager.getLogger();
 
 	/**
 	 * 글 목록
@@ -46,58 +52,73 @@ public class ArticleController {
 	}
 
 	/**
+	 * 글 등록 화면
+	 */
+	@GetMapping("/article/addForm")
+	public String articleAddForm(HttpSession session) {
+		return "article/addForm";
+	}
+
+	/**
 	 * 글 등록
 	 */
 	@PostMapping("/article/add")
-	public String articleAdd(Article article, 
+	public String articleAdd(Article article,
 			@SessionAttribute("MEMBER") Member member) {
 		article.setUserId(member.getMemberId());
 		article.setName(member.getName());
 		articleDao.addArticle(article);
 		return "redirect:/app/article/list";
 	}
-	
-	@GetMapping("/article/updateForm") 
-	public String articleUpdateForm(@RequestParam("articleId") String articleId, 
-			@SessionAttribute("MEMBER") Member member, Model model) { 
-		Article article = articleDao.getArticle(articleId); 
-		if (!article.getUserId().equals(member.getMemberId())) { 
-			return "article/updateFail"; 
-		} 
-		model.addAttribute("article", article); 
-		return "article/updateForm"; 
-	} 
+
+	/**
+	 * 글 수정 화면
+	 */
+	@GetMapping("/article/updateForm")
+	public void updateForm(@RequestParam("articleId") String articleId,
+			@SessionAttribute("MEMBER") Member member, Model model) {
+		Article article = articleDao.getArticle(articleId);
+
+		// 권한 체크 : 세션의 memberId와 글의 userId를 비교
+		if (!member.getMemberId().equals(article.getUserId()))
+			// 자신의 글이 아니면
+			throw new RuntimeException("No Authority!");
+
+		model.addAttribute("article", article);
+	}
 
 	/**
 	 * 글 수정
 	 */
-	@PostMapping("/article/update") 
-	public String articleUpdate(Article article) { 
- 	articleDao.updateArticle(article); 
- 	return "redirect:/app/article/view?articleId=" + article.getArticleId(); 
- 	} 
+	@PostMapping("/article/update")
+	public String update(Article article,
+			@SessionAttribute("MEMBER") Member member) {
+		article.setUserId(member.getMemberId());
+		int updatedRows = articleDao.updateArticle(article);
+
+		// 권한 체크 : 글이 수정되었는지 확인
+		if (updatedRows == 0)
+			// 글이 수정되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
+		return "redirect:/app/article/view?articleId=" + article.getArticleId();
+	}
 
 	/**
 	 * 글 삭제
 	 */
-	@GetMapping("/article/delete") 
-	public String articleDelete(@RequestParam("articleId") String articleId, @SessionAttribute("MEMBER") Member member) { 
-		Article article = articleDao.getArticle(articleId); 
-		if (!article.getUserId().equals(member.getMemberId())) { 
-			return "article/deleteFail"; 
-		} 
-		articleDao.deleteArticle(articleId); 
-		return "article/delete"; 
-	}
-	
-	
-	@PostMapping("/article/updateFail") 
-	public String updateFail() { 
-		return "article/updateFail"; 
-	} 
+	@GetMapping("/article/delete")
+	public String delete(@RequestParam("articleId") String articleId,
+			@SessionAttribute("MEMBER") Member member) {
+		int updatedRows = articleDao.deleteArticle(articleId,
+				member.getMemberId());
 
-	@GetMapping("/article/deleteFail") 
-	public String deleteFail() { 
-		return "article/deleteFail"; 
-	} 
+		// 권한 체크 : 글이 삭제되었는지 확인
+		if (updatedRows == 0)
+			// 글이 삭제되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
+		logger.debug("글을 삭제했습니다. articleId={}", articleId);
+		return "redirect:/app/article/list";
+	}
 }
